9d4d2176a91b5c35bae74ad05c81458a9341b090
Rayne <ellingr2@miamioh.edu>
2024-10-14 20:27:02 -0400

    created balance beam class

---
+++ BalanceBeam.java
@@ -0,0 +1,34 @@
+/**
+ * Class to control the balance beam event.
+ * 
+ * @author Rayne Elling
+ */
+
+
+public class BalanceBeam {
+	// private Skills[];
+	private int totalBeamScore;
+	private int comboScore;
+	private int movesLeft;
+	private String playerChoice;
+	public string roomDescription;
+
+
+	public void loadSkills(){
+
+	}
+
+	public boolean performSkill(){
+		return false;
+	}
+
+	public int calculatePerformance(){
+		return 0;
+	}
+
+
+
+
+
+
+}
