25f3123b85f43ceb42ea46b237f9785885fe05a3
Delano Conigy <delanojobs7@gmail.com>
2024-10-18 14:03:57 -0400

    Defines abstract Move class, unevenBars class.

---
+++ Move.java
@@ -0,0 +1,10 @@
+
+
+public abstract class Move{
+
+    private String name;
+    private String difficulty;
+    private String description;
+
+
+}
---
+++ UnevenBars.java
@@ -0,0 +1,8 @@
+public class UnevenBars {
+
+    public UnevenBars() {
+
+    }
+
+
+}
