934659163648c056ee2ed45faf6793fe931666c8
DakotaOudeman <oudemad@miamioh.edu>
2024-10-30 13:26:40 -0400

    Updated Move class

--- Move.java
+++ Move.java
@@ -1,10 +1,27 @@
 
 
-public abstract class Move{
+public class Move{
 
     private String name;
-    private String difficulty;
+    private Boolean difficulty; // True is hard, false is easy
     private String description;
 
+    public Move(String name, Boolean difficulty, String description) {
+        this.name = name;
+        this.difficulty = difficulty;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Boolean getDifficulty() {
+        return difficulty;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 
 }
