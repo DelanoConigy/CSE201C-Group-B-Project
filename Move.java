193a83dcab7996231a00e58c4be538fd6e63d2a2
Delano Conigy <delanojobs7@gmail.com>
2024-11-01 02:47:04 -0400

    Del worked on his unevenBars class and also added a comment to Move that we can discuss in class later.

--- Move.java
+++ Move.java
@@ -4,7 +4,12 @@ public class Move{
 
     private String name;
     private Boolean difficulty; // True is hard, false is easy
-    private String description;
+    // I feel like changing the difficulty to a number can help determine random events better. 
+    // For instance, if you have a confidence of 50 and the moves difficulty is 75, you could
+    // add the confidence plus a random chance variable to determine whether the move is successful
+    // or not. I think easy and hard is to vague and will make harder implementations when working with
+    // change events.  
+    private String description; 
 
     public Move(String name, Boolean difficulty, String description) {
         this.name = name;
--- UnevenBars.java
+++ UnevenBars.java
@@ -1,7 +1,100 @@
-public class UnevenBars extends Room {
+import java.util.*;
 
-    public   UnevenBars() {
+/**
+ * Class to control the balance beam event.
+ * 
+ * @author Rayne Elling
+ */
+
+
+public class UnevenBars extends Room {
+    //private Skills[];
+    private int elapsedTime;
+    //private int comboScore;
+    private int gripStrength;
+    //private String playerChoice;
+    private String roomDescription;
+    private String instructions;
+    private String moveChoice;
+    private String[] messages = new String[6];
+    private TreeSet<Move> moves = new TreeSet<>();
+
+    // Could be a class named Combo but their will only be 
+    // like three combos and they heavily differ in length
+    private ArrayList<Move> combo1 = new ArrayList<>();
+
+    // an array of ArrayLists, each arrayList is a combo, easy medium hard
+    private ArrayList<Move>[] combos = new ArrayList[3];
+
+
+    public UnevenBars(){
+        this.roomDescription = "Welcome to the balance beam event! Throughout this event you will be provided with choices of which move to make.These moves are dependent on your skill and confidence levels, so choose wisely.";
+        this.instructions = "Please choose the following moves, be wary that each move has a chance of failing!";
+        this.moveChoice = ""; // this will just be the user input
+        this.gripStrength = 20;
+        this.barsScore = 0;
+        this.movesLeft = 6; 
+        this.elapsedTime = 0; // the amount of time you have to perform each move 
+    }
+
+    public void loadMoves(){
+        Move jumpUp = new Move("Jump up", 1.0, "They jump!");
+
+    }
+
+    public boolean performSkill(int skillNum){
+        return false;
+    }
+
+    public void increaseTime(int n){
+        this.elapsedTime += n;
+    }
+
+    public int getTime(){
+        return this.elapsedTime;
+    }
+
+    public String getRoomDescription(){
+        return roomDescription;
+    }
+
+    public String getInstructions(){
+        return null;
+    }
+
+    public void updateScore(double amount){
+        totalBeamScore += amount;
+        setScore(totalBeamScore / count);
+    }
+
+
+    private void storeMessages(){
+        messages[0] = this.roomDescription;
+        messages[1] = this.instructions;
+        messages[2] = this.moveChoice;
+    }
+
+    private void showMessage(int i){
+        System.out.println(messages[i]);
+    }
+    
+    private void displayOptions(Scanner in) {
+
+    }
+
+
+@Override
+    public void startRoom(Player player) {
+        storeMessages();
+        showMessage(0); // Shows welcome message
+        loadMoves();
+        showMessage(1); // Shows instructions
+        //showMessage(2); // Let's begin. Describe start and first move.
+        Scanner scan = new Scanner(System.in);
+        displayOptions(scan);
+        System.out.println(skills[0].getName() + ", " + skills[0].getDifficulty() + ", " + skills[0].getDescription());
         
+        //endRoom();
     }
 
 
