4bd11b50356f3d92ee4c2c7727a67c5099b5cd02
Delano Conigy <delanojobs7@gmail.com>
2024-11-02 17:07:24 -0400

    idk

--- Move.java
+++ Move.java
@@ -4,11 +4,6 @@ public class Move{
 
     private String name;
     private int difficulty; 
-    // I feel like changing the difficulty to a number can help determine random events better. 
-    // For instance, if you have a confidence of 50 and the moves difficulty is 75, you could
-    // add the confidence plus a random chance variable to determine whether the move is successful
-    // or not. I think easy and hard is to vague and will make harder implementations when working with
-    // change events.  
     private String description; 
 
     public Move(String name, int difficulty, String description) {
