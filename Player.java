56cb4deccd7de4c5e3113bbee7db191ded48537d
Rayne <ellingr2@miamioh.edu>
2024-11-03 13:09:25 -0500

    javaDoc for player

--- Player.java
+++ Player.java
@@ -10,6 +10,15 @@ public class Player {
     private String gender;
     private int confidence, points, strength, speed, balance;
 
+    /**
+     * Constructs a Player given gender and skill levels.
+     * 
+     * @param gender of player.
+     * @param confidence level.
+     * @param strength level.
+     * @param speed level.
+     * @param balance level.
+     */
     public Player(String gender, int confidence, int strength, int speed, int balance) {
         this.gender = gender;
         this.confidence = confidence;
@@ -19,6 +28,11 @@ public class Player {
         this.points = 0;
     }
     
+    /**
+     * Constructs a Player given only gender.
+     * 
+     * @param gender of player.
+     */
     public Player(String gender) {
         this.gender = gender;
         this.confidence = 0;
@@ -28,34 +42,74 @@ public class Player {
         this.points = 0;
     }
 
+    /**
+     * Gets the gender of the player
+     * 
+     * @return Gender of player.
+     */
     public String getGender() {
         return gender;
     }
 
+    /**
+     * Gets the speed of the player
+     * 
+     * @return Speed of player.
+     */
     public int getSpeed() {
         return speed;
     }
 
+    /**
+     * Gets the strength of the player
+     * 
+     * @return Strength of player.
+     */
     public int getStrength() {
         return strength;
     }
 
+    /**
+     * Gets the balance of the player
+     * 
+     * @return Balance of player.
+     */
     public int getBalance() {
         return balance;
     }
 
+    /**
+     * Gets the confidence of the player
+     * 
+     * @return Confidence of player.
+     */
     public int getConfidence() {
         return confidence;
     }
 
+    /**
+     * Gets the number of points for the player
+     * 
+     * @return Points of player.
+     */
     public int getPoints() {
         return points;
     }
     
+    /**
+     * Sets the number of points for of the player
+     * 
+     * @param Number of points.
+     */
     public void setPoints(int points) {
     	this.points = points;
     }
 
+    /**
+     * Sets the confidence of the player
+     * 
+     * @param Confidence of player.
+     */
     public void setConfidence(int confidence) {
         if (this.confidence + confidence <= 0) {
             this.confidence = 0;
@@ -66,6 +120,11 @@ public class Player {
         }
     }
 
+    /**
+     * Adjusts the confidence of the player.
+     * 
+     * @param Confidence to adjust by
+     */
     public void addConfidence(int confidence) {
         if (this.confidence + confidence >= 100) {
             this.setConfidence(100);
